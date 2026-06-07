d99c64c046c12272a745d44d3e1d7fee6d4cb981
nece001 <nece001@163.com>
2026-06-07 15:25:28 +0800

    添加插件安装基类

---
+++ src/PlugInstall.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Nece\Framework\Adapter;
+
+class PlugInstall
+{
+    const WEBMAN_PLUGIN = true;
+
+    /**
+     * @var array
+     */
+    protected static array $pathRelation = [];
+
+    /**
+     * Install
+     * @return void
+     */
+    public static function install()
+    {
+        static::installByRelation();
+    }
+
+    /**
+     * Uninstall
+     * @return void
+     */
+    public static function uninstall()
+    {
+        self::uninstallByRelation();
+    }
+
+    /**
+     * installByRelation
+     * @return void
+     */
+    public static function installByRelation()
+    {
+        foreach (static::$pathRelation as $source => $dest) {
+            $config_path = base_path() . "/$dest";
+            if(!is_dir($config_path)){
+                mkdir($config_path, 0777, true);
+            }
+
+
+            echo $source . "\n";
+            
+            copy_dir($source, $config_path);
+            echo "Create $dest\n";
+        }
+    }
+
+    /**
+     * uninstallByRelation
+     * @return void
+     */
+    public static function uninstallByRelation()
+    {
+        foreach (static::$pathRelation as $source => $dest) {
+            $path = base_path() . "/$dest";
+            if (!is_dir($path) && !is_file($path)) {
+                continue;
+            }
+            echo "Remove $dest
+";
+            if (is_file($path) || is_link($path)) {
+                unlink($path);
+                continue;
+            }
+            remove_dir($path);
+        }
+    }
+}
